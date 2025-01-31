0fb02b0bac45c64f16b3311db02b11a88e87b24c
Shubhamsk2000 <shubhamkondhalkar2003@gmail.com>
2025-02-01 00:19:19 +0530

    fix import statement for useSignup hook in SignUp component

--- frontend/src/pages/pagesIndex.js
+++ frontend/src/pages/pagesIndex.js
@@ -1,6 +1,6 @@
 import Login from "./Login.jsx";
 import Home from "./Home.jsx"
-import SignUp from "./signup/SignUp.jsx";
+import SignUp from "./SignUp/SignUp.jsx";
 
 export default Home;
 export {
