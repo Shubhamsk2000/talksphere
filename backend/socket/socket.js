70bf1779eb1866e56b03450f247f547d1f372bef
Shubhamsk2000 <shubhamkondhalkar2003@gmail.com>
2025-02-01 13:02:54 +0530

    fix socket.io server initialization by correcting server reference

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -4,7 +4,7 @@ import http from 'http';
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(Server);
+const io = new Server(server);
 
 const userSocketMap = {};
 
